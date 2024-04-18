d58ecca5d23782347b7ec6ffd97ef5b45739c752
Jose Ramirez <joseramirezgomez00@gmail.com>
2024-04-18 17:01:09 -0500

    v-if,v-else,v-for

--- vue-project1/src/App.vue
+++ vue-project1/src/App.vue
@@ -1,47 +1,53 @@
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const mensaje ='Vue diámico';
+const styleColor = {color: 'blue'};
+const arraycolor = ['red','blue','green'];
+const activo = true;
+const arrayFrutas = ['🍇', '🍉', '🍌','🍓'];
+const arrayFrutas2 = [
+  {
+    nombre:'Manzana',
+    precio: 100,
+    descripcion: 'Una Manzana'
+  },
+  {
+    nombre:'Pera',
+    precio: 300,
+    descripcion: 'Una Pera'
+  },
+  {
+    nombre:'Banana',
+    precio: 200,
+    descripcion: 'Una Banana'
+  }
+]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>Hola {{mensaje.toUpperCase() }}!</h1>
+  <!--los : es la abrebiacion del v-bind-->
+  <h2 :style="`color:${arraycolor[1]}`">Soy azul</h2>
+  <h2>
+    {{ activo ? "Estoy activo" : "Estoy inactivo" }}
+  </h2>
+  <p v-if="activo">Estoy activo</p>
+  <p v-else>Estoy inactivo</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h2 v-show="activo">Estoy activo v-show</h2>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="fruta in arrayFrutas2" :key="fruta.nombre">{{ fruta.nombre }} -- ${{ fruta.precio }} -- {{ fruta.descripcion }}</li>
+  </ul>
+
+
+  <ul>
+    <li v-for="(fruta, index) in arrayFrutas" :key="index">{{index}} - {{ fruta }}</li>
+  </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+h1{
+  color: red;
 }
 </style>
