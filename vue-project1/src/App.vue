211467cd001bff1a18c90321c476eeb08eb290ef
Jose Ramirez <joseramirezgomez00@gmail.com>
2024-04-19 11:07:27 -0500

    recorriendo elementos de forma optima con v-for y v-if

--- vue-project1/src/App.vue
+++ vue-project1/src/App.vue
@@ -1,37 +1,39 @@
-
 <script setup>
-const mensaje ='Vue diámico';
-const styleColor = {color: 'blue'};
-const arraycolor = ['red','blue','green'];
+const mensaje = 'Vue diámico';
+const styleColor = { color: 'blue' };
+const arraycolor = ['red', 'blue', 'green'];
 const activo = true;
-const arrayFrutas = ['🍇', '🍉', '🍌','🍓'];
+const arrayFrutas = ['🍇', '🍉', '🍌', '🍓'];
 const arrayFrutas2 = [
   {
-    nombre:'Manzana',
+    nombre: 'Manzana',
     precio: 100,
-    descripcion: 'Una Manzana'
+    descripcion: 'Una Manzana',
+    stock: 10,
   },
   {
-    nombre:'Pera',
+    nombre: 'Pera',
     precio: 300,
-    descripcion: 'Una Pera'
+    descripcion: 'Una Pera',
+    stock: 0,
   },
   {
-    nombre:'Banana',
+    nombre: 'Banana',
     precio: 200,
-    descripcion: 'Una Banana'
+    descripcion: 'Una Banana',
+    stock: 20,
   }
 ];
 
 const objetoFruta = {
-    nombre:'Pera',
-    precio: 300,
-    descripcion: 'Una Pera'
-  } 
+  nombre: 'Pera',
+  precio: 300,
+  descripcion: 'Una Pera'
+} 
 </script>
 
 <template>
-  <h1>Hola {{mensaje.toUpperCase() }}!</h1>
+  <h1>Hola {{ mensaje.toUpperCase() }}!</h1>
   <!--los : es la abrebiacion del v-bind-->
   <h2 :style="`color:${arraycolor[1]}`">Soy azul</h2>
   <h2>
@@ -43,24 +45,37 @@ const objetoFruta = {
   <h2 v-show="activo">Estoy activo v-show</h2>
 
   <ul>
-    <li v-for="fruta in arrayFrutas2" :key="fruta.nombre">{{ fruta.nombre }} -- ${{ fruta.precio }} -- {{ fruta.descripcion }}</li>
+    <li v-for="fruta in arrayFrutas2" :key="fruta.nombre">{{ fruta.nombre }} -- ${{ fruta.precio }} -- {{
+      fruta.descripcion }}</li>
   </ul>
 
 
   <ul>
-    <li v-for="(fruta, index) in arrayFrutas" :key="index">{{index}} - {{ fruta }}</li>
+    <li v-for="(fruta, index) in arrayFrutas" :key="index">{{ index }} - {{ fruta }}</li>
   </ul>
 
   <!-- recorriendo objetos -->
   <ul>
     <li v-for="(value, propiedad) in objetoFruta" :key="value">
-    {{ propiedad }} : {{ value }}
+      {{ propiedad }} : {{ value }}
     </li>
+  </ul>
+
+  <h2>v-for y v-if</h2>
+  <!-- recorriendo elementos de forma optima con v-for y v-if -->
+
+  <ul>
+    <template v-for="item in arrayFrutas2" :key="item.nombre">
+      <li v-if="item.stock > 0">
+        {{ item.nombre }} - {{ item.precio }}
+
+      </li>
+    </template>
   </ul>
 </template>
 
 <style>
-h1{
+h1 {
   color: red;
 }
 </style>
